617cf3d5708989f3aa7049840d62bd7a068f834d
Dang Khoa <dangkhoa080998@gmail.com>
2022-10-26 15:29:39 +0700

    add stage 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import SearchIcon from './search.svg';
+import MovieCard from "./MovieCard";
+const API_URL = 'https://api.themoviedb.org/3';
+const API_KEY = '9a5f6f8e54a84fb4f6444d2a4b8a4a84';
+const App = () => {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [search, setSearch] = useState('');
+    const [movies, setMovies] = useState([]);
+
+    const searchMovies = async (title) => {
+        let uri = title ? '/search/movie' : '/movie/popular'
+        const response = await fetch(`${API_URL}${uri}?query=${title}&api_key=${API_KEY}`);
+        const data = await response.json();
+        setMovies(data.results);
+    }
+    useEffect(() => {
+        searchMovies('Batman');
+    }, [])
+
+    return (
+        <div className='app'>
+            <h1>Movies Review</h1>
+
+            <div className='search'>
+                <input
+                    placeholder='Search for Movies'
+                    value={search}
+                    onChange={(e) => setSearch(e.target.value)} />
+                <img
+                    src={SearchIcon}
+                    alt='search'
+                    onClick={() => searchMovies(search)} />
+            </div>
+            {movies.length > 0 ? (
+                <div className='container'>
+                    {movies.map((movie) => (
+                        <MovieCard key={movie.id} movie={movie} />
+                    ))}
+                </div>
+            ) : (
+                <div className='empty'>
+                    <h2>No movie found</h2>
+                </div>
+            )}
+        </div>
+    );
 }
 
-export default App;
+export default App
---
+++ src/MovieCard.jsx
@@ -0,0 +1,29 @@
+import starIcon from './star.svg';
+const IMAGE_URL = 'https://image.tmdb.org/t/p';
+
+const IMAGES_SIZES = {
+    small: `${IMAGE_URL}/w185/`,
+    medium: `${IMAGE_URL}/w342/`,
+    large: `${IMAGE_URL}/w500/`,
+    extraLarge: `${IMAGE_URL}/original/`,
+}
+
+const MovieCard = ({ movie }) => {
+    let { release_date, poster_path, title, original_title, vote_average } = movie;
+    return (
+        <div className='movie'>
+            <div>
+                <p>{release_date}</p>
+            </div>
+            <div>
+                <img src={poster_path ? IMAGES_SIZES.medium + poster_path : 'https://via.placeholder.com/400' } alt={title} />
+            </div>
+            <div>
+                <span>{vote_average} <img src={starIcon} alt="star"/></span>
+                <h3>{original_title}</h3>
+            </div>
+        </div>
+    )
+}
+
+export default MovieCard
